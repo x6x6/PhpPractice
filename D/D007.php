a14b5005f9cfa69955786f2f290d93abc7fd287c
s-kimura <ostkimura1995@gmail.com>
2016-05-11 18:12:59 +0900

    paiza

---
+++ D/D007.php
@@ -0,0 +1,8 @@
+<?php
+  $N = fgets(STDIN);
+
+  for($i=0;$i<$N;$i++){
+    echo "*";
+  }
+  echo "\n";
+?>
---
+++ D/D011.php
@@ -0,0 +1,5 @@
+<?php
+$s = fgets(STDIN);
+
+echo ord($s) - 65 + 1; //asciiコード"A"の６５を元に何番目かを計算
+?>
