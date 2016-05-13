8e998cf93606efd055719c677884219d452d431a
s-kimura <ostkimura1995@gmail.com>
2016-05-13 18:23:13 +0900

    paiza 20160513

--- C029.php
+++ C029.php
@@ -6,28 +6,41 @@ $n = $stdin1[1]; //旅行の日数
 
 $start = -99999;
 $end = 0;
-$rainfall_prob = 100;
+$persent = 100;
 
 for($i=0;$i<$m;$i++){
   $w_data[$i] = explode(' ', trim(fgets(STDIN)));
 }
 
+// var_dump($w_data);
+// echo $n;
+  // echo $m;
+// echo $w_data[0][0];
+// echo $w_data[$m-1][0];
+
 for($i=0;$i<$m;$i++){
-  if($i + $n > $m){ //旅行終了日が連休の日数を過ぎていたら強制的に終わらす
+  if($i+$n > $m){
+    // echo $i;
     break;
   }
   $ttl = 0;
-  for($j=0;$j<$n;$j++){ //降水確率の合計を求める
+
+  for($j=0;$j<$n;$j++){
     $ttl += $w_data[$i+$j][1];
   }
-  if($start < $w_data[$i][0]){ //？
-    if($rainfall_prob >= (int)($ttl / $n)){　//降水確率の平均が今までのより引くかったら
+
+// echo $i.":".(int)($ttl / $n)."\n";
+
+  if($start < $w_data[$i][0]){
+    if($persent >= (int)($ttl / $n)){
       $start = $w_data[$i][0];
       $end = $w_data[$i+($j-1)][0];
-      $rainfall_prob = (int)($ttl / $n);
+      $persent = (int)($ttl / $n);
     }
   }
 }
 
 echo $start." ".$end."\n";
+
+// var_dump($percent);
 ?>
